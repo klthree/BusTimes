c4562eb9c1dfc9fe3601b6ebd60b700daa72c954
zen <klthree@users.noreply.github.com>
2020-06-27 00:12:46 -0400

    New functionality and name changes
    Renamed bus_routes.js to pat.js, with "pat" short for "Port Authority
    Transit." Adjusted folder structure to improve organization.
    Added functionality to pat.js:
    get_stops accepts a route, direction, and rtpi and returns an array of stops.
    get_vehicles accepts a route and returns a list of vehicles currently
    operating on that route.
    get_dir accepts an array of routes and adds another property to each
    element of that array specifying the possible directions. For Pittsburgh
    it's trivial; there's only INBOUND and OUTBOUND.
    get_routes accepts, but does not require, a search term, searches all
    available routes, and returns an array of routes with matching names. If search term
    is an empty string get_routes returns an array of all known routes.

--- web-server/src/bus-app.js
+++ web-server/src/bus-app.js
@@ -2,15 +2,17 @@
 
 const path = require('path');
 const express = require('express');
-const bus_routes = require('./bus_routes');
+const bus_routes = require('./utils/pat');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/bus', async (req, res) => {
     loc = "";
-    let x = await bus_routes.get_routes(loc);
-    res.send(x);
+    let x = await bus_routes.get_routes(loc).then((response) => bus_routes.get_dir(response));
+    let y = await bus_routes.get_stops(8, "OUTBOUND", "Port Authority Bus");
+    let z = await bus_routes.get_vehicles(6);
+    res.send(z);
 })
 
 app.listen(3000, () => {
