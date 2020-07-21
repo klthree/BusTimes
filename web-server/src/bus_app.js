c2eaf8f0be108743b1a8aa4e56f924b2ffb0452b
zen <klthree@users.noreply.github.com>
2020-07-21 02:50:52 -0400

    Coarse pathfinding implemented

--- web-server/src/bus_app.js
+++ web-server/src/bus_app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const init = require('./utils/init');
 const process = require('./utils/process');
-const ras = require ('./data/routes_and_stops.json')
+const ras = require ('./data/routes_and_stops2.json')
 const all_stops = require ('./data/all_stops.json');
 
 const app = express();
@@ -16,36 +16,37 @@ app.set('views', views);
 app.get('/bus', (req, res) => {
     
     const start = {
-        latitude: 40.444498,
-        longitude:  -80.000726
+        latitude: 40.444384,
+        longitude: -80.000594
     }
     const dest = {
-        latitude: 40.463261,
-        longitude: -79.991807
+        latitude: 40.463034,
+        longitude: -79.991610
     }
 
-    const near_start = process.closest_stops(start, all_stops, 5);
-    const near_dest = process.closest_stops(dest, all_stops, 5);
+    // const near_start = process.closest_stops(start, all_stops, 5);
+    // const near_dest = process.closest_stops(dest, all_stops, 5);
 
-    const to_take = [];
+    // const to_take = [];
     
-    // https://stackoverflow.com/a/32961666
-    near_start.sorted_stops.forEach(stop_s => {
-        console.log(stop_s);
-        near_dest.sorted_stops.forEach(stop_d => {
-            stop_s.routes.forEach(rt_s => {
-                stop_d.routes.forEach(rt_d => {
-                    if(JSON.stringify(rt_s) === JSON.stringify(rt_d)) {
-                        to_take.push({rtnm: rt_s, start: stop_s.stpnm, destination: stop_d.stpnm});
-                    }
-                })
-            })
-        })
-    })
+    // // https://stackoverflow.com/a/32961666
+    // near_start.sorted_stops.forEach(stop_s => {
+    //     console.log(stop_s);
+    //     near_dest.sorted_stops.forEach(stop_d => {
+    //         stop_s.routes.forEach(rt_s => {
+    //             stop_d.routes.forEach(rt_d => {
+    //                 if(JSON.stringify(rt_s) === JSON.stringify(rt_d)) {
+    //                     to_take.push({rtnm: rt_s, start: stop_s.stpnm, destination: stop_d.stpnm});
+    //                 }
+    //             })
+    //         })
+    //     })
+    // })
 
-    // let result = init.routes_per_stop();
+    let result = process.pathfinder(start, dest);
+    // let result = process.adjacency(ras[6], ras[8]);
 
-    res.send(to_take);
+    res.send(result);
 
     // loc = "";
 
