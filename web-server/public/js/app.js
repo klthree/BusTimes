09ecc00c2fb06455d53ea212ad7d694ad401c465
zen <klthree@users.noreply.github.com>
2021-03-30 17:11:39 -0400

    styles.css file, client-side javascript file app.js added.

---
+++ web-server/public/js/app.js
@@ -0,0 +1,22 @@
+const coordForm = document.querySelector('#loc-search');
+const search = document.querySelector('input');
+// document.querySelector('')
+coordForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const place = search.value;
+    // console.log(place);
+    // window.open('http://localhost:3000/bus?loc=' + place, "_self");
+
+    fetch('http://localhost:3000/bus?loc=' + place).then((response) => {
+        response.json().then((data) => {
+            console.log("testy");
+            console.log(data);
+        })
+    })
+})
+
+// fetch('http://localhost:3000/?loc=pittsburgh').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data);
+//     })
+// })
