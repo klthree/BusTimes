0cd6fb82860e3475106edcbe45bc530833f07742
zen <klthree@users.noreply.github.com>
2021-03-30 20:40:06 -0400

    Cleaned up some unecessary comments.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,11 +5,9 @@ const coords = document.querySelector('#results');
 coordForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const place = search.value;
-    console.log(place);
 
     fetch('http://localhost:3000/bus?loc=' + place).then((response) => {
         response.json().then((data) => {
-            console.log(data);
             let latitude = data.coordinates[1];
             let longitude = data.coordinates[0];
             coords.textContent = "Coordinates: " + latitude + ", " + longitude;
