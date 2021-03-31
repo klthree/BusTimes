a1a332d6ec9a80daf08399dc753d095ba2e4e515
zen <klthree@users.noreply.github.com>
2021-03-30 20:01:51 -0400

    Removed require statements that don't work in client-side javascript.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,6 +1,3 @@
-const validator = require("@mapbox/mapbox-sdk/services/service-helpers/validator");
-const { routes_per_stop } = require("../../src/utils/init");
-
 const coordForm = document.querySelector('#loc-search');
 const search = document.querySelector('input');
 // document.querySelector('')
@@ -8,14 +5,15 @@ coordForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const place = search.value;
     // console.log(place);
-    window.open('http://localhost:3000/bus?loc=' + place, "_self");
+    // window.open('http://localhost:3000/bus?loc=' + place, "_self");
 
-    // fetch('http://localhost:3000/bus?loc=' + place).then((response) => {
-    //     response.json().then((data) => {
-    //         console.log("testy");
-    //         console.log(data);
-    //     })
-    // })
+    fetch('http://localhost:3000/bus?loc=' + place).then((response) => {
+        console.log(response);
+        // response.json().then((data) => {
+        //     console.log("testy");
+        //     console.log(data);
+        // })
+    })
 })
 
 // fetch('http://localhost:3000/?loc=pittsburgh').then((response) => {
