b4537a35621c9588c14d611a6e6a2937b363d09b
zen <klthree@users.noreply.github.com>
2021-04-02 19:27:22 -0400

    Added JSDoc comment, changed return value
    Now returns coordinates in a format immediately usable by the rest of
    the functions in module.

--- web-server/src/utils/geocoder.js
+++ web-server/src/utils/geocoder.js
@@ -2,16 +2,23 @@ const mapkey = require('./keys.js').mapboxKey;
 const forwardGeocodeClient = require('@mapbox/mapbox-sdk/services/geocoding');
 const geocodeSvc = forwardGeocodeClient({accessToken: mapkey});
 
+/**
+ * Given a location string, uses the Mapbox javascript module to return coordinates.
+ * 
+ * @param {string} loc 
+ * @returns {object} - {lat, lon}
+ */
 const getCoords = async (loc) => {
     try {
+        console.log("In getCoords, loc = " + loc);
         let response = await geocodeSvc.forwardGeocode({query: loc, limit: 1}).send();
-        // console.log(response.body);
-        // console.log("test: " + typeof response.body.features[0].center);
-        // console.log("Features length = " + response.body.features.length);
         if (response.body.features.length == 0) {
             return "Error: Invalid placename";
         } else {
-            return response.body.features[0].center;
+            let coordinates = {lat: response.body.features[0].center[1], lon: response.body.features[0].center[0]}
+            console.log("In getCoords, result = " + coordinates.lat + ", " + coordinates.lon);
+            return coordinates;
+
         }
     } catch (error) {
         console.error(error);
